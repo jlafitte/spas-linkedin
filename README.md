717d2c047bef9b0e416579460c32aea21dbabf15
John Lafitte <john.lafitte@gmail.com>
2015-12-08 01:05:59 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# spas-linkedin
+LinkedIn API helpers for SPAS
+
+## Installation
+
+### Installing spas
+``` bash
+$ [sudo] npm install spas -g
+```
+
+### Installing spas-linkedin
+``` bash
+$ npm install spas-linkedin
+```
+
+## Documentation
+Your bundle should have an item like this:
+
+"linkedin": {
+  "resource": linkedin.companyScrape,
+  "params": {
+    "url": "https://www.linkedin.com/company/joyent",
+    "count": 3
+  },
+  "cacheduration": 3000,
+  "timeout": 5000
+}
+
+Just change the URL to the company linkedin page, and the count to the number of entires you'd like to return.
---
+++ api.js
@@ -0,0 +1,41 @@
+/*jshint node:true,expr:true*/
+'use strict';
+
+var spashttp = require("spas-http");
+var request = require('request');
+var cheerio = require('cheerio');
+
+exports.companyScrape = function(params, credentials, cb){
+  request(params.url, function(err, response, body) {
+    var $ = cheerio.load(body);
+    var items = [];
+    $('li.feed-item').each(function(idx,li){
+      if (params.count && idx < params.count) {
+        var $li = $(this);
+        var item = {};
+        item.id = $li.attr('data-li-update-id');
+        item.date = $li.attr('data-li-update-date');
+        // item.niceDate = new Date(parseInt(this.datestamp)).toISOString();
+        var $img = $li.find('img').first();
+        item.image = {};
+        item.image.src = $img.attr('src') || $img.attr('data-li-lazy-load-src');
+        item.image.alt = $img.attr('alt');
+
+        item.content = $li.find('.share-body').text();
+        items.push(item);
+      } else {
+        return false;
+      }
+    });
+    cb(err, items);
+  });
+};
+
+exports.companyFeed = function(params, credentials, cb){
+  if (params.access_token) {
+    params.oauth2_access_token = credentials.access_token;
+  }
+  spashttp.request(params, credentials, function (err, response) {
+    cb(err, response);
+  });
+};
